ec0510427aa7794b0bb909f60d78f354c0f7da48
sunsee83 <sunsee83@gmail.com>
2026-08-27 17:25:27 +0900

    Remove obsolete single-file app entrypoint

--- reels-inspector/app.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  'use strict';
-  window.__REELS_INSPECTOR_REMOTE__ = { version: '0.1.0', loadedAt: Date.now() };
-  console.log('[Reels Inspector] remote app loaded', window.__REELS_INSPECTOR_REMOTE__.version);
-})();
