b5aa95e909a3b47b500b6d63f5ecae5c06a4af16
sunsee83 <sunsee83@gmail.com>
2026-08-27 17:10:55 +0900

    Move Instagram extension app entry

---
+++ extension-ig-research/app.js
@@ -0,0 +1,5 @@
+(() => {
+  'use strict';
+  window.__REELS_INSPECTOR_REMOTE__ = { version: '0.1.0', loadedAt: Date.now() };
+  console.log('[Reels Inspector] remote app loaded', window.__REELS_INSPECTOR_REMOTE__.version);
+})();
