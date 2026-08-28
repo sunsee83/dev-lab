73351518cdb5cfc041617fceb47e4fdc126a96d7
sunsee83 <sunsee83@gmail.com>
2026-08-28 14:04:03 +0900

    Release Reels Inspector 2.2.7

--- reels-inspector/ri-retry.user.js
+++ reels-inspector/ri-retry.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Reels Inspector Mobile
 // @namespace    dev-lab/reels-inspector
-// @version      2.2.6
+// @version      2.2.7
 // @match        *://*.instagram.com/*
 // @grant        none
 // @run-at       document-start
 // @require      https://raw.githubusercontent.com/sunsee83/dev-lab/20c293c807f6a8129cbae5df19ff0b53e8ce7664/reels-inspector/ri-retry.user.js
 // @require      https://raw.githubusercontent.com/sunsee83/dev-lab/c0de52926f42a417f251ed31a97195be50ad8d8f/reels-inspector/ri-reels-hotfix-225.js
 // @require      https://raw.githubusercontent.com/sunsee83/dev-lab/ccc901116d182524cf89b5943e8e49ac9d76c766/reels-inspector/ri-reels-hotfix-226.js
+// @require      https://raw.githubusercontent.com/sunsee83/dev-lab/3c7a22a696e68ec33583df03c5eb34bc41a5885c/reels-inspector/ri-reels-hotfix-227.js
 // @updateURL    https://github.com/sunsee83/dev-lab/raw/refs/heads/main/reels-inspector/ri-retry.user.js
 // @downloadURL  https://github.com/sunsee83/dev-lab/raw/refs/heads/main/reels-inspector/ri-retry.user.js
 // ==/UserScript==
